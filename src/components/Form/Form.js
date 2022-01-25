b4b2ba84228bc41756c343e8a92496902cf2026b
Eric Li <ericli@knights.ucf.edu>
2022-01-24 22:10:19 -0700

    Create initial Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { fetchData } from '../../apiCalls';
+// import Select from 'react-select';
 
 const Form = (props) => {
+
+  const [formData, setFormData] = useState(null);
+  const [occupationValue, setOccupationValue] = useState(null)
+
+  useEffect(() => {
+    fetchOccupationLocationData()
+  }, [])
+
+  const fetchOccupationLocationData = () => {
+    fetchData("https://frontend-take-home.fetchrewards.com/form")
+      .then(data => setFormData(data))
+  }
+
+  const occupationOptions = formData ? formData.occupations.map((occupation, index) => {
+    return (
+      <option className="occupation-option" key={index} value={occupation}></option>
+    )
+  }) : null
+
+  const handleChange = (event) => {
+    setOccupationValue(event.target.value)
+  } 
+
   return (
-    <div>
-      
-    </div>
+    <form>
+      <input type="text" placeholder="Full Name"></input>
+      <input type="text" placeholder="Email"></input>
+      <input type="password" placeholder="Password"></input>
+      <select className="occupation-select" onChange={handleChange} name="occupation">{formData && occupationOptions}</select>
+    </form>
   );
 }
 
