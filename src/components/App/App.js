084d2341b01ec356fb4382d74d3d2d175f88c41e
Eric Li <ericli@knights.ucf.edu>
2022-01-24 22:10:02 -0700

    Import Form component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import Form from '../Form/Form';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Form />
       </header>
     </div>
   );
