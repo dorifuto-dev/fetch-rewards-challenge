2fef354f865394c905bbb72ed6ceb5555c2ff0e1
Eric Li <ericli@knights.ucf.edu>
2022-01-26 14:39:05 -0700

    Create initial Cypress tests, delete example tests

---
+++ cypress/integration/App_spec.js
@@ -0,0 +1,15 @@
+describe('Fetch Rewards User flows', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('Should have a homepage URL', () => {
+    cy.url('http://localhost:3000/')
+  })
+
+  it('Should display a logo and a form', () => {
+    cy.get('.app-logo').should('exist')
+    cy.get('.main-form').should('exist')
+  })
+})
