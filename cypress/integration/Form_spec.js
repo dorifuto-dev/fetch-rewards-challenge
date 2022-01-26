c54d79d2c6e86de7af25f317631a99f16394877f
Eric Li <ericli@knights.ucf.edu>
2022-01-26 15:24:49 -0700

    Refactor stubbing, add custom Cypress commands

--- cypress/integration/Form_spec.js
+++ cypress/integration/Form_spec.js
@@ -1,7 +1,7 @@
 describe('Fetch Rewards Form User flows', () => {
 
   beforeEach(() => {
-    cy.intercept('GET', 'https://frontend-take-home.fetchrewards.com/form', { fixture: 'formData.json' })
+    cy.interceptGetData()
     cy.visit('http://localhost:3000/')
   })
 
@@ -41,28 +41,8 @@ describe('Fetch Rewards Form User flows', () => {
   })
 
   it('Should display a success message on successful completion of the form', () => {
-    cy.intercept('POST', 'https://frontend-take-home.fetchrewards.com/form', {
-      statusCode: 200,
-      body: {
-        name: 'Eric Li',
-        email: 'samplemail@sample.org',
-        password: '123456789',
-        occupation: 'Interim Substitute Teacher',
-        state: 'AR'
-      }
-    })
-    cy.get('.name-input')
-      .type('Eric Li')
-    cy.get('.email-input')
-      .type('samplemail@sample.org')
-    cy.get('.password-input')
-      .type('123456789')
-    cy.get('.occupation-select')
-      .select(2)
-    cy.get('.location-select')
-      .select(4)
-    cy.get('.submit-button')
-      .click()
+    cy.interceptHappySubmit()
+    cy.fillForm()
     cy.get('.notification')
       .should('contain', 'Successfully signed up. Welcome to Fetch Rewards!')
   })
@@ -70,41 +50,14 @@ describe('Fetch Rewards Form User flows', () => {
   // SAD PATH TESTING
 
   it('Should display a notification if a user fails to fill out the form completely', () => {
-    cy.get('.name-input')
-      .type('Eric Li')
-    cy.get('.email-input')
-      .type('samplemail@sample.org')
-    cy.get('.password-input')
-      .type('123456789')
-    cy.get('.submit-button')
-      .click()
+    cy.fillIncompleteForm()
     cy.get('.notification')
       .should('contain', 'Please fill out all fields before submitting the form.')
   })
 
   it('Should display notification of a server error upon submission of the form', () => {
-    cy.intercept({
-      method: 'POST',
-      url: 'https://frontend-take-home.fetchrewards.com/form'
-    },
-    {
-      statusCode: 500,
-      body: {
-        message: '500 Server timed out.'
-      }
-    })
-    cy.get('.name-input')
-      .type('Eric Li')
-    cy.get('.email-input')
-      .type('samplemail@sample.org')
-    cy.get('.password-input')
-      .type('123456789')
-    cy.get('.occupation-select')
-      .select(2)
-    cy.get('.location-select')
-      .select(4)
-    cy.get('.submit-button')
-      .click()
+    cy.interceptSadSubmit()
+    cy.fillForm()
     cy.get('.notification')
       .should('contain', 'Something went wrong. Please try submitting the form again.')
   })
